eae91a975ab975b3506fbb933cbf9ed4a8c0ba83
Russell Chien <RussellChien6@gmail.com>
2021-10-31 02:32:36 -0700

    updated files

--- yfinance_adjusted_closing_price.py
+++ yfinance_adjusted_closing_price.py
@@ -3,15 +3,33 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-class yfinance_adjusted_closing_price(object):
-    def __init__(self, ticker, start_date, end_date):
-        self.ticker = ticker
-        self.start_date = start_date
-        self.end_date = end_date
+class Chart(object):
+    def __init__(self, x_length, y_length, title, x_label, y_label, title_font_size, label_font_size, color, linestyle, linewidth):
+        self.x_length = x_length
+        self.y_length = y_length
+        self.title = title
+        self.x_label = x_label
+        self.y_label = y_label
+        self.title_font_size = title_font_size
+        self.label_font_size = label_font_size
+        self.color = color
+        self.linestyle = linestyle
+        self.linewidth = linewidth
+
+    def create_chart(self, data):
+        data.plot(figsize=(self.x_length,self.y_length))
+        plt.legend()
+        plt.title(self.title, fontsize=self.title_font_size)
+        plt.ylabel(self.x_label, fontsize=self.label_font_size)
+        plt.xlabel(self.y_label, fontsize=self.label_font_size)
+        plt.grid(which = 'major', color = self.color, linestyle = self.linestyle, linewidth = self.linewidth)
+        plt.show()
 
 if __name__ == '__main__':
     addstock = True
     ticker_list = []
+
+    # user input 
     while (addstock) :
         ticker = input('What stock(s) would you like to get market data on? Enter "done" when done entering tickers. ')
         ticker = ticker.upper()
@@ -19,22 +37,15 @@ if __name__ == '__main__':
             addstock = False
         else:
             ticker_list.append(ticker)
-
     date_range = input('Please enter a date range with the format YYYY-MM-DD,YYYY-MM-DD: ')
     start_date = date_range[:10]
     end_date = date_range[11:]
-    # data = yf.download(ticker, start_date, end_date)
-    data = pd.DataFrame(columns=ticker_list)
 
+    # downloading data 
+    data = pd.DataFrame(columns=ticker_list)
     for ticker in ticker_list:
         data[ticker] = yf.download(ticker, start_date, end_date)['Adj Close']
 
-    data.head()
-    data.plot(figsize=(10,7))
-    plt.legend()
-    # plt.title('Adjusted Close Price of %s' %ticker.upper(), fontsize=16)
-    plt.title('Adjusted Close Price', fontsize = 16)
-    plt.ylabel('Price', fontsize=14)
-    plt.xlabel('Year', fontsize=14)
-    plt.grid(which = 'major', color = 'k', linestyle = '-', linewidth = .5)
-    plt.show()
+    # creating chart
+    adj_close_price = Chart(10, 7, 'Adjusted Close Price', 'Price', 'Year', 16, 14, 'k', '-', .5)
+    adj_close_price.create_chart(data)
